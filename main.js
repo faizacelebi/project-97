8aabefb72fa78fa1cc46f92403c42b6765e04b5c
faizacelebi <84246436+faizacelebi@users.noreply.github.com>
2022-07-18 14:44:41 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,44 @@
+function preview1(){
+    window.location.href = "https://www.google.com/books/edition/White_Fang/G5lcbFxUqI8C?hl=en&gbpv=1&dq=white+fang&printsec=frontcover";
+}
+
+function preview2(){
+    window.location.href = "https://www.google.com/books/edition/Red_Rover/RHX0DwAAQBAJ?hl=en&gbpv=1&dq=red+rover&printsec=frontcover";
+}
+
+function preview3(){
+    window.location.href = "https://www.google.com/books/edition/The_Sacrifice_of_Sunshine_Girl/HRo9DQAAQBAJ?hl=en&gbpv=1&dq=The+Sacrifice+of+Sunshine+girl&printsec=frontcover";
+}
+
+function preview4(){
+    window.location.href = "https://www.google.com/books/edition/Spy_Penguins/8yBGDwAAQBAJ?hl=en&gbpv=1&printsec=frontcover";
+}
+
+function preview5(){
+    window.location.href = "https://www.google.com/books/edition/Judy_Moody_M_D/a2UIrkvwoM8C?hl=en&gbpv=1&printsec=frontcover";
+}
+
+function preview6(){
+    window.location.href = "https://www.google.com/books/edition/Zach_King_My_Magical_Life/Z0XTDQAAQBAJ?hl=en&gbpv=1&printsec=frontcover";
+}
+
+function preview7(){
+    window.location.href = "https://www.google.com/books/edition/Restart/uf1rDQAAQBAJ?hl=en&gbpv=1&printsec=frontcover";
+}
+
+function preview8(){
+    window.location.href = "https://www.google.com/books/edition/Scream_of_the_Evil_Genie_Give_Yourself_G/VHXMBgAAQBAJ?hl=en&gbpv=1&printsec=frontcover";
+}
+
+function preview9(){
+    window.location.href = "https://www.goodreads.com/book/show/125592.Tick_Tock_You_re_Dead_";
+}
+
+function preview10(){
+    window.location.href = "https://www.google.com/books/edition/Goosebumps_Let_s_Get_Invisible/eerXCQAAQBAJ?hl=en&gbpv=1&printsec=frontcover";
+}
+
+function preview11(){
+    window.location.href = "https://www.google.com/books/edition/Give_Yourself_Goosebumps_Trapped_in_Bat/LSjABQAAQBAJ?hl=en&gbpv=1&printsec=frontcover";
+}
+
